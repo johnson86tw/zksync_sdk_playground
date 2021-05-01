a76ea6644c415d9d124c5c5ab125d5460f595bf1
陳梓瑋 <chnejohnson@gmail.com>
2021-05-01 18:12:55 +0800

    update signSyncTransfer

--- signSyncTransfer.js
+++ signSyncTransfer.js
@@ -7,7 +7,7 @@ const { submitSignedTransactionsBatch } = wallet;
 
 require("dotenv").config();
 
-// wallet2 transfer 0.004 ETH to wallet without gas fee which is paid by wallet
+// wallet2 transfer 0.0001 ETH to wallet without gas fee which is paid by wallet
 
 const privateKey = process.env.ZKSYNC_DEV_PRIVATE_KEY;
 const privateKey2 = process.env.ZKSYNC_DEV2_PRIVATE_KEY;
@@ -27,20 +27,32 @@ const privateKey2 = process.env.ZKSYNC_DEV2_PRIVATE_KEY;
     syncProvider
   );
 
-  const tx = await syncWallet.signSyncTransfer({
-    to: syncWallet2.address(),
+  // wallet 2 signed transaction
+  const tx = await syncWallet2.signSyncTransfer({
+    to: syncWallet.address(),
     token: "ETH",
     amount: ethers.utils.parseEther("0.001"),
+    fee: BigNumber.from("0"),
+    nonce: await syncWallet2.getNonce(),
+  });
+
+  // wallet signed transaction just for gas fee
+  const tx2 = await syncWallet.signSyncTransfer({
+    to: syncWallet.address(),
+    token: "ETH",
+    amount: ethers.utils.parseEther("0"),
     fee: await syncProvider.getTransactionsBatchFee(
-      ["Transfer"],
-      [syncWallet2.address()],
+      ["Transfer", "Transfer"],
+      [syncWallet.address(), syncWallet.address()],
       "ETH"
     ),
     nonce: await syncWallet.getNonce(),
   });
 
-  const batchTxs = await submitSignedTransactionsBatch(syncProvider, [tx]);
+  // submit signed transactions batch
+  const batchTxs = await submitSignedTransactionsBatch(syncProvider, [tx, tx2]);
   await batchTxs[0].awaitReceipt();
+  await batchTxs[1].awaitReceipt();
 
   const committedETHBalance = await syncWallet.getBalance("ETH");
   console.log(
